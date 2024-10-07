41050542c2b46b5a4e195ec241108f7e3726e813
Mathis Mahaux <mathis.mahaux@gmail.com>
2024-10-07 15:24:31 +0200

    Adds a 'Search by id' functionnality

--- BdAngular/src/app/app.component.ts
+++ BdAngular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ComicListComponent} from "./comic-list/comic-list.component";
 import {AddComicComponent} from "./add-comic/add-comic.component";
+import {ComicDetailComponent} from "./comic-detail/comic-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComicListComponent, AddComicComponent],
+  imports: [RouterOutlet, ComicListComponent, AddComicComponent, ComicDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
